88c4d0fda3f99ea8687b0181027055164fddb13d
Benny Özcetin <benny@five-star.dev>
2025-09-30 22:37:45 +0200

    Update Tracker screen that shows the position and can start the tracker

--- app/src/main/java/dev/five_star/trackingapp/features/tracker/presentation/TrackerAction.kt
+++ app/src/main/java/dev/five_star/trackingapp/features/tracker/presentation/TrackerAction.kt
@@ -2,4 +2,6 @@ package dev.five_star.trackingapp.features.tracker.presentation
 
 sealed interface TrackerAction {
     data object OnTrackerClicked: TrackerAction
+    data class UpdateZoom(val zoom: Float): TrackerAction
+    data object OnPermissionGranted: TrackerAction
 }
--- app/src/main/java/dev/five_star/trackingapp/features/tracker/presentation/TrackerState.kt
+++ app/src/main/java/dev/five_star/trackingapp/features/tracker/presentation/TrackerState.kt
@@ -4,6 +4,8 @@ import com.google.android.gms.maps.model.LatLng
 
 data class TrackerState(
     val gpsStrength: GpsStrength = GpsStrength.NO_SIGNAL,
+    val gpsValue: Double = 0.0,
     val isTracking: Boolean = false,
-    val position: LatLng? = null,
+    val location: LatLng? = null,
+    val zoom: Float = 0f
 )
