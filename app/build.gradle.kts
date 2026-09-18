b2cd8da0fac0b8c1345ddf03adccd467afcbdcfc
Benny Özcetin <benny@five-star.dev>
2026-09-18 17:40:58 +0200

    update agp to 9.4.0 and gradle to 9.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ import java.io.InputStreamReader
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.gms.google.services)
 }
